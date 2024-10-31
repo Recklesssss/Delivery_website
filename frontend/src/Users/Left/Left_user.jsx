44c7186d06a4e9404fd5bd28d180ae21246d9c06
reckless <biruktesfa444@gmail.com>
2024-10-31 11:29:34 +0300

    adjusting left toggle for the adding user page

---
+++ frontend/src/Users/Left/Left_user.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Left_user.css"
+
+function Left_user() {
+  return (
+    <div className='left_user'>
+        <div className="admin_profile">
+            <img className = "admin_pfp" src="" alt="" />
+            <p>Admin name</p>
+        </div>
+        div.admin
+    </div>
+  )
+}
+
+export default Left_user
