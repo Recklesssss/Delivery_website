c1c03e481481ce53865e37ce246b209e92738dd9
reckless <biruktesfa444@gmail.com>
2024-11-01 08:05:02 +0300

    configuring the link and style

--- frontend/src/Users/AddUser.jsx
+++ frontend/src/Users/AddUser.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header_user from './Header/Header_user'
 import Left_user from './Left/Left_user'
+import Main_user from './Main/Main_user'
+
 
 function AddUser() {
   return (
     <div>
         <Header_user/>
         <Left_user/>
+        <Main_user/>
     </div>
   )
 }
